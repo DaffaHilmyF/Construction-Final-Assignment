0f2ae998fbaaaef4aa5726e9fbbc007f5e74be44
nengannishasetyawati <55216332+nengannishasetyawati@users.noreply.github.com>
2021-06-26 16:40:42 +0700

    Menambah konfigurasi
    Menambah konfigurasi untuk defensive programming

---
+++ TUBES_FINAL/Config/HandlingConfig.cs
@@ -0,0 +1,64 @@
+namespace TUBES_FINAL.Config
+{
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Text.RegularExpressions;
+
+    public static class HandlingConfig
+    {
+        public static void SanitizeFileIsExist(string inputParams)
+        {
+            if (!File.Exists(inputParams))
+                throw new FileNotFoundException("File not found");
+        }
+
+        public static void SanitizeInputNotNull(string inputParams)
+        {
+            if (String.IsNullOrEmpty(inputParams))
+                throw new ArgumentNullException("Input should not be blank!");
+        }
+
+        public static void SanitizeInputNotNull<T>(List<T> inputParams)
+        {
+            if (inputParams == null)
+                throw new ArgumentNullException("Input should not be blank!");
+        }
+
+        public static void SanitizeInputLengthIsCorrect(string inputParams, int exceptedLength)
+        {
+            if (inputParams.Length != exceptedLength)
+                throw new FormatException("Input length should be 10");
+        }
+
+        public static void SanitizeInputIsNumeric(string inputParams)
+        {
+            if (int.TryParse(inputParams, out _) == false)
+                throw new FormatException("Input not numeric!");
+        }
+
+        public static void SanitizeInputIsNumeric(params string[] inputParams)
+        {
+            foreach(var item in inputParams)
+            {
+                if (int.TryParse(item, out _) == false)
+                    throw new FormatException("Input not numeric!");
+            }
+            
+        }
+
+        public static void SanitizeInputNotOverflow(int inputParams)
+        {
+            if ((inputParams > Int32.MaxValue) || (inputParams < Int32.MinValue))
+                throw new OverflowException("Input is overflow");
+        }
+
+
+
+        public static void SanitizeInputNotNegative(int inputParams)
+        {
+            if (inputParams < 0)
+                throw new OverflowException("Input value is cannot below zero");
+        }
+    }
+}
