d1673353afdb27e7f69eba6abbd494a30de25773
Fathuddin Ashari <55197547+DaffaHilmyF@users.noreply.github.com>
2021-06-28 22:16:51 +0700

    adding method from controller

--- TUBES_FINAL/Controller/LoginController.cs
+++ TUBES_FINAL/Controller/LoginController.cs
@@ -5,6 +5,9 @@
     using System.Linq;
     using System.Text;
     using System.Threading.Tasks;
+    using TUBES_FINAL.Config;
+    using TUBES_FINAL.Database;
+    using TUBES_FINAL.Model;
     using TUBES_FINAL.View;
 
     public class LoginController
@@ -17,7 +20,34 @@
             LoginView.Show();
         }
 
+        
+        public void MoveToDashboard(string email, string password)
+        {
+            HandlingConfig.SanitizeInputNotNull(email, password);
 
+            List<LecturerModel> lecturerList = DBLecturer.GetAllLecturer();
+            List<StudentModel> studentList = DBStudent.GetAllStudent();
+            string indexUser;
+
+            foreach(var item in lecturerList)
+            {
+                if(item.PersonEmail.CompareTo(email) == 0)
+                {
+                    if (AccountConfig.IsPasswordCorrect(item.PersonPassword, password))
+                        indexUser = item.PersonID;
+                }
+            }
+
+            foreach(var item in studentList)
+            {
+                if (item.PersonEmail.CompareTo(email) == 0)
+                {
+                    if (AccountConfig.IsPasswordCorrect(item.PersonPassword, password))
+                        indexUser = item.PersonID;
+                }
+            }
+
+        }
 
     }
 }
--- TUBES_FINAL/View/LoginView.Designer.cs
+++ TUBES_FINAL/View/LoginView.Designer.cs
@@ -29,24 +29,102 @@ namespace TUBES_FINAL.View
         /// </summary>
         private void InitializeComponent()
         {
+            this.EmailField = new System.Windows.Forms.TextBox();
+            this.PasswordField = new System.Windows.Forms.TextBox();
+            this.B_Login = new System.Windows.Forms.PictureBox();
+            this.B_Register_Student = new System.Windows.Forms.PictureBox();
+            this.B_Register_Lecturer = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.B_Login)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.B_Register_Student)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.B_Register_Lecturer)).BeginInit();
             this.SuspendLayout();
+            // 
+            // EmailField
+            // 
+            this.EmailField.BackColor = System.Drawing.SystemColors.Window;
+            this.EmailField.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.EmailField.ForeColor = System.Drawing.Color.Black;
+            this.EmailField.Location = new System.Drawing.Point(494, 125);
+            this.EmailField.Name = "EmailField";
+            this.EmailField.Size = new System.Drawing.Size(242, 15);
+            this.EmailField.TabIndex = 0;
+            // 
+            // PasswordField
+            // 
+            this.PasswordField.BackColor = System.Drawing.SystemColors.Window;
+            this.PasswordField.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.PasswordField.ForeColor = System.Drawing.Color.Black;
+            this.PasswordField.Location = new System.Drawing.Point(494, 184);
+            this.PasswordField.Name = "PasswordField";
+            this.PasswordField.PasswordChar = '*';
+            this.PasswordField.Size = new System.Drawing.Size(242, 15);
+            this.PasswordField.TabIndex = 1;
+            // 
+            // B_Login
+            // 
+            this.B_Login.BackgroundImage = global::TUBES_FINAL.Properties.Resources.Button_Login;
+            this.B_Login.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.B_Login.Location = new System.Drawing.Point(656, 221);
+            this.B_Login.Name = "B_Login";
+            this.B_Login.Size = new System.Drawing.Size(92, 37);
+            this.B_Login.TabIndex = 2;
+            this.B_Login.TabStop = false;
+            this.B_Login.Click += new System.EventHandler(this.B_Login_Click);
+            // 
+            // B_Register_Student
+            // 
+            this.B_Register_Student.BackgroundImage = global::TUBES_FINAL.Properties.Resources.Button_StudentRegister;
+            this.B_Register_Student.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.B_Register_Student.Location = new System.Drawing.Point(481, 262);
+            this.B_Register_Student.Name = "B_Register_Student";
+            this.B_Register_Student.Size = new System.Drawing.Size(112, 57);
+            this.B_Register_Student.TabIndex = 3;
+            this.B_Register_Student.TabStop = false;
+            this.B_Register_Student.Click += new System.EventHandler(this.B_Register_Student_Click);
+            // 
+            // B_Register_Lecturer
+            // 
+            this.B_Register_Lecturer.BackgroundImage = global::TUBES_FINAL.Properties.Resources.Button_LecturerRegister;
+            this.B_Register_Lecturer.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.B_Register_Lecturer.Location = new System.Drawing.Point(636, 264);
+            this.B_Register_Lecturer.Name = "B_Register_Lecturer";
+            this.B_Register_Lecturer.Size = new System.Drawing.Size(112, 57);
+            this.B_Register_Lecturer.TabIndex = 4;
+            this.B_Register_Lecturer.TabStop = false;
+            this.B_Register_Lecturer.Click += new System.EventHandler(this.B_Register_Lecturer_Click);
             // 
             // LoginView
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
             this.BackgroundImage = global::TUBES_FINAL.Properties.Resources.Background_Login1;
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.ClientSize = new System.Drawing.Size(782, 403);
+            this.Controls.Add(this.B_Register_Lecturer);
+            this.Controls.Add(this.B_Register_Student);
+            this.Controls.Add(this.B_Login);
+            this.Controls.Add(this.PasswordField);
+            this.Controls.Add(this.EmailField);
             this.DoubleBuffered = true;
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Name = "LoginView";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Welcome";
+            ((System.ComponentModel.ISupportInitialize)(this.B_Login)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.B_Register_Student)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.B_Register_Lecturer)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox EmailField;
+        private System.Windows.Forms.TextBox PasswordField;
+        private System.Windows.Forms.PictureBox B_Login;
+        private System.Windows.Forms.PictureBox B_Register_Student;
+        private System.Windows.Forms.PictureBox B_Register_Lecturer;
     }
 }
