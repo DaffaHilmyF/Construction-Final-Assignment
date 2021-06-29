f064f753c8a01d995dd4da24eb4c0aee2deb72e4
Fathuddin Ashari <55197547+DaffaHilmyF@users.noreply.github.com>
2021-06-29 15:56:17 +0700

    Unit test configuration

---
+++ LMS_UNIT_TEST/ConfigTest/AccountConfigTest.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using TUBES_FINAL.Config;
+
+namespace LMS_UNIT_TEST.ConfigTest
+{
+    [TestClass]
+    class AccountConfigTest
+    {
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException), "Input should not be blank!")]
+        public void Should_ThrowException_When_Input_IsDomainEmailLecturerCorrect_Null()
+        {
+            string InitialInput = null;
+            AccountConfig.IsDomainEmailLecturerCorrect(InitialInput);
+
+        }
+    }
+}
