af855d0e57deb4134ecfe99547dd950502a3e881
Kevin Reihan Ghazali Adam <55223583+kevinpakev@users.noreply.github.com>
2021-07-04 18:35:34 +0700

    update

--- TUBES_FINAL/Controller/RegisterLecturerController.cs
+++ TUBES_FINAL/Controller/RegisterLecturerController.cs
@@ -28,7 +28,7 @@ namespace TUBES_FINAL.Controller
 
         internal void CreateAccount(string name, string nidn, string email, string password) 
         {
-            HandlingConfig.SanitizeInputNotNull<string>(name, nidn, email, password);
+            HandlingConfig.SanitizeInputNotNull(name, nidn, email, password);
             HandlingConfig.SanitizeInputIsNumeric(nidn);
             HandlingConfig.SanitizeInputLengthIsCorrect(nidn, 10);
             AccountConfig.IsDomainEmailLecturerCorrect(email);
